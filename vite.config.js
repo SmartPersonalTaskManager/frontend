1f66827f591bb4d9163b614238b8c9c0c014e753
sonergunes741 <sonergunes741@gmail.com>
2025-12-21 13:13:03 +0300

    Configure for GitHub Pages deployment: Update base path and API URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SPTM/", // Base path for GitHub Pages matching Repo Name 'SPTM'
+  base: "/web/", // Base path for GitHub Pages matching Repo Name 'web'
   server: {
     port: 5173,
     strictPort: true, // Hata versin başka port kullanmaya çalışırsa
