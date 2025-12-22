643f6a9785faf88010f5ad629b70087dcd912c71
Beyza Yılmaz <beyzanuryilmaz3773@gmail.com>
2025-12-22 16:58:23 +0300

    updated config for publishing

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontend/", // Update to match the URL in your screenshot
-  server: {
-    port: 5173,
-    strictPort: true, // Hata versin başka port kullanmaya çalışırsa
-  },
-});
+  base: '/'
+})
